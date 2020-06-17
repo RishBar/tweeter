873fee4b082daf59700638be9733c992ddac391d
Rishav Baral <rishavbaral6@gmail.com>
2020-06-17 03:07:09 +0000

    add hardcoded tweet box into the html and style it according the example given, add hover effect over this tweet box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,16 +2,12 @@ $(document).ready(function() {
   const $tweet = $('#tweet-text');
   $tweet.on('keyup', function() {
     let charLeft = (140 - (this.value.length));
-    // console.log(this.parentNode.childNodes[3].childNodes[3]);
-    // const counterNode = this.parentNode.childNodes[3].childNodes[3];
-    // $('counter').html("ayyyy");
-    let charCount = $('.counter');
-    charCount.text(charLeft);
+    let $charCount = $('.counter');
+    $charCount.text(charLeft);
     if (charLeft < 0) {
-      $('.counter').css('color', 'red');
-    }
-    if (charLeft >= 0) {
-      $('.counter').css('color', '#545149');
+      $charCount.css('color', 'red');
+    } else {
+      $charCount.css('color', '#545149');
     }
   });
 });
---
+++ public/scripts/tweet-hover.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   const tweetBox = $('article');
+//   tweetBox.on('mouseover', function() {
+//     $('article').css('box-shadow', '5px 10px');
+//   });
+//   tweetBox.on('mouseleave', function() {
+//     $('article').css('box-shadow', '');
+//   });
+// });
