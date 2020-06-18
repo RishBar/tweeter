e65785b4a3aae806b37fb32632e27e6926cb3f8f
Rishav Baral <rishavbaral6@gmail.com>
2020-06-17 17:57:12 -0700

    Implement a createTweetElement function, that returns html for a rendered tweet. Implement renderTweets function that iterates over tweet data in /tweets/ and then calls createTweetElement on each iteration. then create an AJAX POST request in your client.js that sends the form data to the server. define a function called loadTweets that is responsible for fetching tweets from the /tweets page. Implement validation before sending the form data to the server.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,63 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+$(document).ready(function() {
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      let $tweetElement = createTweetElement(tweet);
+      $('#tweets-container').append($tweetElement);
+    }
+  };
+
+  const createTweetElement = function(tweet) {
+    const tweetElement = `
+      <article class="one-tweet">
+        <div class="avatar-name-handle">
+          <div class="avatar-name">
+            <img src=${tweet["user"]["avatars"]}> 
+            <p>${tweet["user"]["name"]}</p>
+          </div>
+          <p>${tweet["user"]["handle"]}</p>
+        </div>
+        <p class="tweet-content">${tweet["content"]["text"]}</p>
+        <div class="days-ago-edit">
+          <p>${tweet["created_at"]}</p>
+          <p>some buttons</p>
+        </div>
+      </article>
+    `;
+    return tweetElement;
+  };
+
+  $('.form-container').on('submit', function(event) {
+    event.preventDefault();
+    const tweetData = $(this).serialize();
+    const decodedTweet = decodeURIComponent(tweetData).substring(5)
+    if (!decodedTweet) {
+      alert("That's an empty string");
+    } else if (decodedTweet.length > 140) {
+      alert("That tweets to long buddy");
+    } else {
+      console.log(decodeURIComponent(tweetData).substring(5));
+      $.post('/tweets/',
+        tweetData,
+        function() {
+          console.log('success');
+        });
+    }
+  });
+
+  const loadTweets = function() {
+    $.ajax('/tweets/', { method: 'GET' })
+      .then(function (loadedTweets) {
+        console.log('Success: ', loadedTweets);
+        renderTweets(loadedTweets);
+      });
+  };
+
+  loadTweets();
+
+});
